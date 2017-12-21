d4f95aa1727965842319a7c5891e8b261506ac1f
unknown <inikolovbiz@gmail.com>
2017-12-21 21:34:12 +0100

    da

--- dbtest.js
+++ dbtest.js
@@ -1,5 +1,5 @@
 
-var knexmssql = require('knex')({
+let knexmssql = require('knex')({
   client: 'mssql',
   connection: {
     server     : '192.168.7.210',
@@ -10,8 +10,25 @@ var knexmssql = require('knex')({
   }  
 });
 
+let bookshelfMSSQL = require('bookshelf')(knexmssql);
+
+let TestsitesMSSQL = bookshelfMSSQL.Model.extend({
+  tableName: 'testforums',
+  idAttribute: 'forumid'
+});
+
+let testsiteMSSQL = new TestsitesMSSQL();
 
 knexmssql.raw(`select forumid, siteid, forumlink, fname, forumnumber from testforums
 where siteid = '3b13ad7f108';`).then(function (response) {
     console.log(response[0].forumlink)
+})
+
+
+testsiteMSSQL.where('siteid', '3b13ad7f108').fetchAll()
+.then(function(siteMSSQL) {
+  let resultsMSSQl = siteMSSQL.toJSON();
+  console.log(resultsMSSQl.length + ' MS SQL testsites records fetched');
+}).finally(function () {
+    process.exit()
 })
